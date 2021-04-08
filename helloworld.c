ce497929391b8804f409cfecb1b8da03917a738d
Mircea Pop <mirceapop28@gmail.com>
2021-04-08 02:32:06 -0400

    Additional update to wellcome message
    Signed-off-by: Mircea Pop <mirceapop28@gmail.com>

--- helloworld.c
+++ helloworld.c
@@ -3,7 +3,8 @@ int main() {
 	int counter = 0;
 
 	for (counter; counter < 100 ; counter ++){
-		printf("Wellcome to mamaia %d \n ", counter);
+	
+		printf("Wellcome to MAMAIA %d \n ", counter);
 	}
    return 0;
 }
